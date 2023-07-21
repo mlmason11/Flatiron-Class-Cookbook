4059ddc607249bc7238e29f9a6ee356d42cd6aa9
Max <mlmason11@gmail.com>
2023-07-21 16:48:34 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// GLOBAL CONSTANTS
+
+// EVENT LISTENERS 
+
+// FUNCTIONS
+
+// DEFAULT IMAGE RANDOMIZER 
+
+// create a dropdown menu using select and options tags to feature meal types.
+    // when meal type is selected, recipies list will render in recipie container
+
+    // when receipe is clicked, populates card with receipe details and image
+
+// when user submits form, comment populates comments section 
+    // like button 
