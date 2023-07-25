3ff416087d6d1e0be4d6a7550714fa07ffe66333
Max <mlmason11@gmail.com>
2023-07-25 14:37:03 -0400

    Finished get/post for comment section

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ const ingredients = document.getElementById('ingredients')
 const instructions = document.getElementById('instructions')
 const allergens = document.getElementById('allergens')
 const rating = document.getElementById('rating')
-
 const ratingForm = document.getElementById('rating-form')
 const stars = document.getElementById('stars')
 const featuredImage = document.getElementById('featured-image')
@@ -34,15 +33,136 @@ const newName = document.querySelector('#new-name')
 const newComment = document.querySelector('#new-comment')
 
 
-// optiona container 
 
-//const optionsContainer = document.createElement('section')
-//const listOptionsDiv = document.createElement('div')
-//optionsContainer.append(listOptionsDiv)
-//const categorySelect = document.createElement('select')
-//listOptionsDiv.append(categorySelect)
+function addOneComment(commentObj) {
+    const parentBlock = document.createElement('div')
+    parentBlock.className = "parent-block grid"
+    commentContainer.append(parentBlock)
 
+    //targeting selectors (name + comment)
 
+    const userName = document.createElement('small')
+    userName.setAttribute('class', 'user-name col-12')
+    userName.style.alignContent = 'left'
+    userName.textContent = commentObj.username
+
+    const userComment = document.createElement('p')
+    userComment.setAttribute('class', 'user-comment col-12')
+    userComment.textContent = commentObj.comment
+
+    const heart = document.createElement('i')
+    heart.setAttribute('class', 'heart col-12')
+    heart.classList.add('fa-regular' ,'fa-heart')
+
+    // Heart Button event listener
+    heart.addEventListener('click', e => {
+        e.preventDefault()
+        console.log('hearted')
+        if (heart.classList.contains('fa-solid')) {
+            heart.classList.remove('fa-solid' , 'fa-heart')
+            heart.classList.add('fa-regular' , 'fa-heart')
+        }
+        else if (heart) {
+            heart.classList.contains('fa-regular' ,'fa-heart')
+            heart.classList.add('fa-solid', 'fa-heart')
+        }
+    })
+
+    // const userIcon =  document.createElement("div")
+    // userIcon.className = "col-2"
+    
+    parentBlock.append(heart, userName, userComment)
+}
+
+// Function to add one recipe to the list
+function addOneRecipe(recipeObj) {
+    const recipeItem = document.createElement('li')
+    recipeItem.textContent = recipeObj.name
+    recipeItem.addEventListener('click', e => {
+        e.preventDefault()
+        populateDetails(recipeObj)
+    })
+    listParent.append(recipeItem)    
+}
+
+// Populates the recipe card with the details of the clicked recipe
+// Takes in a recipe object and uses its data to fill in the card
+function populateDetails(recipeObj) {
+
+    title.innerHTML=''
+    detailImage.innerHTML=''
+    description.innerHTML=''
+    ingredients.innerHTML=''
+    instructions.innerHTML=''
+    allergens.innerHTML=''
+    rating.innerHTML=''
+    commentContainer.innerHTML=''
+
+    currentRecipe = recipeObj
+
+    const recipeName = document.createElement('h1')
+    recipeName.textContent = recipeObj.name
+    title.append(recipeName)
+
+    const image = document.createElement('img')
+    image.src = recipeObj.image
+    detailImage.append(image)
+
+    const starNum = document.createElement('p')
+    starNum.textContent = `${recipeObj.stars / recipeObj.ratings}`
+    //stars.textContent = `${currentRecipe.stars / currentRecipe.ratings} Stars`
+    rating.append(starNum)
+
+    const descr = document.createElement('p')
+    descr.textContent = recipeObj.description
+    description.append(descr)
+
+    const ingr = document.createElement('p')
+    ingr.textContent = recipeObj.ingredients
+    ingredients.append(ingr)
+
+    const instr = document.createElement('p')
+    instr.textContent = recipeObj.steps
+    instructions.append(instr)
+
+    const allerg = document.createElement('p')
+    allerg.textContent = recipeObj.allergens
+    allergens.append(allerg)
+
+    // fetch statement to get all the comments for recipeObj and display them in the comment container
+    // in the fetch statement, we will use addOneComment recursively to add all comments for given recipeObj
+    fetch(`http://localhost:3000/comments`)
+    .then(r => r.json())
+    .then(commentArray => {
+        commentArray.forEach(commentObj => {
+            if (currentRecipe.id === commentObj.refno) {
+                addOneComment(commentObj)
+            }
+        })
+    }).catch(error => alert(error))
+}
+
+// fetch(`http://localhost:3000/recipes`)
+// .then(response => response.json())
+// .then(recipeArray => {
+//     recipeArray.forEach(recipeObj => addOneRecipe(recipeObj))
+// }).catch(error => alert(error))
+
+categoryMenu.addEventListener ('change', e => {
+    currentCategory = e.target.value
+    console.log(currentCategory)
+    listParent.innerHTML=""
+    fetch(`http://localhost:3000/recipes`)
+    .then(response => response.json())
+    .then(recipeArray => {
+        
+        recipeArray.forEach(recipeObj => {
+            if (recipeObj.category === currentCategory) {
+                addOneRecipe(recipeObj)
+            }
+        })
+    }).catch(error => alert(error))
+})
 
 ratingForm.addEventListener('submit', e => {
     e.preventDefault()
@@ -69,9 +189,8 @@ ratingForm.addEventListener('submit', e => {
     }
 })
 
-
 // Comments form event listener to submit comments, and add them to the database for each recipe
-commentForm.addEventListener('submit', (e) => {
+commentForm.addEventListener('submit', e => {
     e.preventDefault()
     console.log('clicked')
     console.log(e.target.value)
@@ -89,136 +208,12 @@ commentForm.addEventListener('submit', (e) => {
                 'refno': currentRecipe.id
             })   
         }).then(response => response.json())
-        .then(commentObj => console.log(commentObj))
+        .then(commentObj => addOneComment(commentObj))
         .catch(error => alert(error))
     }
     else {
         console.log(`Please select a recipe to comment on it`)
     }
-})
-    
-//     function addOneComment(commentObj) {
-        
-        
-        
-//         newComment.textContent = commentObj.comment
 
-//     const parentBlock = document.createElement('div')
-//     parentBlock.className = "parent-block grid"
-//     commentContainer.append(parentBlock)
-
-//     //targeting selectors (name + comment)
-
-//     const userName = document.createElement('small')
-//     userName.textContent = newName.value
-//     userName.setAttribute('class', 'user-name col-12')
-//     userName.style.alignContent = 'left'
-
-//     const userComment = document.createElement('p')
-//     userComment.setAttribute('class', 'user-comment col-12')
-//     userComment.textContent = newComment.value
-
-//     const heart = document.createElement('i')
-//     heart.setAttribute('class', 'heart col-12')
-//     heart.classList.add('fa-regular' ,'fa-heart')
-
-//     // Heart Button event listener
-    
-//     heart.addEventListener('click', () => {
-//         console.log('click')
-//         if (heart.classList.contains('fa-solid')) {
-//             heart.classList.remove('fa-solid' , 'fa-heart')
-//             heart.classList.add('fa-regular' , 'fa-heart')
-//         }
-//         else if (heart) {
-//             heart.classList.contains('fa-regular' ,'fa-heart')
-//             heart.classList.add('fa-solid', 'fa-heart')
-//         }
-//     })
-//     const userIcon =  document.createElement("div")
-//     userIcon.className = "col-2"
-    
-
-//     parentBlock.prepend(userName, userComment, heart, userIcon)
-
-//     commentForm.reset()
-// }
-
-// Populates the recipe card with the details of the clicked recipe
-// Takes in a recipe object and uses its data to fill in the card
-function populateDetails(recipeObj) {
-    currentRecipe = recipeObj
-
-    const recipeName = document.createElement('h1')
-    recipeName.textContent = recipeObj.name
-    title.append(recipeName)
-
-    const image = document.createElement('img')
-    image.src = recipeObj.image
-    detailImage.append(image)
-
-    const starNum = document.createElement('p')
-    starNum.textContent = `${recipeObj.stars / recipeObj.ratings}`
-    //stars.textContent = `${currentRecipe.stars / currentRecipe.ratings} Stars`
-    rating.prepend(starNum)
-
-    const descr = document.createElement('p')
-    descr.textContent = recipeObj.description
-    description.append(descr)
-
-    const ingr = document.createElement('p')
-    ingr.textContent = recipeObj.ingredients
-    ingredients.append(ingr)
-
-    const instr = document.createElement('p')
-    instr.textContent = recipeObj.steps
-    instructions.append(instr)
-
-    const allerg = document.createElement('p')
-    allerg.textContent = recipeObj.allergens
-    allergens.append(allerg)
-
-    
-}
-
-
-
-
-// Function to add one recipe to the list
-function addOneRecipe(recipeObj) {
-    const recipeItem = document.createElement('li')
-    recipeItem.textContent = recipeObj.name
-    recipeItem.addEventListener('click', e => {
-        e.preventDefault()
-        title.innerHTML=''
-        detailImage.innerHTML=''
-        description.innerHTML=''
-        ingredients.innerHTML=''
-        instructions.innerHTML=''
-        allergens.innerHTML=''
-        rating.innerHTML=''
-        populateDetails(recipeObj)
-    })
-    listParent.append(recipeItem)    
-}
-
-// fetch(`http://localhost:3000/recipes`)
-// .then(response => response.json())
-// .then(recipeArray => {
-//     recipeArray.forEach(recipeObj => addOneRecipe(recipeObj))
-// }).catch(error => alert(error))
-
-categoryMenu.addEventListener ('change', e => {
-    currentCategory = e.target.value
-    listParent.innerHTML=""
-    fetch(`http://localhost:3000/recipes`)
-    .then(response => response.json())
-    .then(recipeArray => {
-        
-        recipeArray.forEach(recipeObj => {
-            if (recipeObj.category === currentCategory) {
-                addOneRecipe(recipeObj)
-            }
-        })
-    }).catch(error => alert(error))
+    commentForm.reset()
 })
