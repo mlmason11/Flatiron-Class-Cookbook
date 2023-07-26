12e2bc8c1558b4087a2c3c3074a8e0bd973f6c98
Max <mlmason11@gmail.com>
2023-07-26 15:43:03 -0400

    Tried to dynamically create rating form

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ const instructions = document.getElementById('instructions')
 const listParent = document.getElementById('list-parent')
 const newComment = document.querySelector('#new-comment')
 const newName = document.querySelector('#new-name')
+const rateFormContainer = document.getElementById('rate-form-container')
 const rating = document.getElementById('rating')
-const ratingForm = document.getElementById('rating-form')
-const stars = document.getElementById('stars')
+//const ratingForm = document.getElementById('rating-form')
 const title = document.getElementById('title')
 
 
@@ -39,8 +39,6 @@ function addOneComment(commentObj) {
     const parentBlock = document.createElement('div')
     parentBlock.className = "parent-block grid"
     commentContainer.append(parentBlock)
-
-    //targeting selectors (name + comment)
 
     const userName = document.createElement('small')
     userName.setAttribute('class', 'user-name col-12')
@@ -89,7 +87,6 @@ function addOneRecipe(recipeObj) {
 // Populates the recipe card with the details of the clicked recipe
 // Takes in a recipe object and uses its data to fill in the card
 function populateDetails(recipeObj) {
-
     title.innerHTML=''
     detailImage.innerHTML=''
     description.innerHTML=''
@@ -109,11 +106,6 @@ function populateDetails(recipeObj) {
     image.src = recipeObj.image
     detailImage.append(image)
 
-    const starNum = document.createElement('p')
-    starNum.textContent = `${recipeObj.stars / recipeObj.ratings}`
-    //stars.textContent = `${currentRecipe.stars / currentRecipe.ratings} Stars`
-    rating.append(starNum)
-
     const descr = document.createElement('p')
     descr.textContent = recipeObj.description
     description.append(descr)
@@ -130,6 +122,69 @@ function populateDetails(recipeObj) {
     allerg.textContent = recipeObj.allergens
     allergens.append(allerg)
 
+    const avgStars = document.createElement('p')
+    const totalRatings = document.createElement('p')
+    if (recipeObj.ratings === 0) {
+        avgStars.textContent = `This recipe has not been rated yet`
+        rating.append(avgStars)
+    }
+    else {
+        
+        avgStars.textContent = `${recipeObj.stars / recipeObj.ratings} Stars`
+        totalRatings.textContent = `${recipeObj.ratings} Rating(s)`
+        rating.append(avgStars, totalRatings)
+    }
+
+    const ratingForm = document.createElement("form");
+    ratingForm.setAttribute("method", "patch");
+    ratingForm.setAttribute("action" , "submit");
+
+    const ratingLabel = document.createElement('label')
+    ratingLabel.setAttribute('for', 'enter-rating')
+    ratingLabel.textContent = 'Your rating:'
+
+    const ratingInput = document.createElement('input')
+    ratingInput.id = 'enter-rating'
+    ratingInput.setAttribute('type', 'number')
+    ratingInput.setAttribute('min', '0')
+    ratingInput.setAttribute('max', '5')
+    ratingInput.setAttribute('step', '0.5')
+
+    const ratingSubmit = document.createElement('input')
+    ratingSubmit.setAttribute('type', 'submit')
+    ratingSubmit.setAttribute('class', 'rate-button')
+    ratingSubmit.setAttribute('value', 'Rate')
+
+    ratingForm.append(ratingLabel, ratingInput, ratingSubmit)
+    ratingForm.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(`http://localhost:3000/recipes${currentRecipe.id}`, {
+            'method': "PATCH",
+            'header': {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            'body': JSON.stringify({
+                'ratings': ++currentRecipe.ratings,
+                'stars': currentRecipe.stars + e.target.value
+            })
+        }).then(response => response.json())
+        .then(newRating => {return(newRating)})
+        .catch(error => alert(error))
+        if (recipeObj.ratings === 0) {
+            avgStars.textContent = `This recipe has not been rated yet`
+        }
+        else if (recipeObj.ratings === 1) {
+            avgStars.textContent = `${recipeObj.stars} Stars`
+            totalRatings.textContent = `${recipeObj.ratings} Rating`
+        }
+        else {
+            avgStars.textContent = `${recipeObj.stars / recipeObj.ratings} Stars`
+            totalRatings.textContent = `${recipeObj.ratings} Ratings`
+        }
+    })
+    rateFormContainer.append(ratingForm)
+
     // fetch statement to get all the comments for recipeObj and display them in the comment container
     // in the fetch statement, we will use addOneComment recursively to add all comments for given recipeObj
     fetch(`http://localhost:3000/comments`)
@@ -143,8 +198,6 @@ function populateDetails(recipeObj) {
     }).catch(error => alert(error))
 }
 
-
-
 categoryMenu.addEventListener ('change', e => {
     currentCategory = e.target.value
     console.log(currentCategory)
@@ -152,7 +205,6 @@ categoryMenu.addEventListener ('change', e => {
     fetch(`http://localhost:3000/recipes`)
     .then(response => response.json())
     .then(recipeArray => {
-        
         recipeArray.forEach(recipeObj => {
             if (recipeObj.category === currentCategory) {
                 addOneRecipe(recipeObj)
@@ -161,37 +213,11 @@ categoryMenu.addEventListener ('change', e => {
     }).catch(error => alert(error))
 })
 
-ratingForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const ratingValue = e.target.value
-    if (ratingValue >= 0 && ratingValue <= 5) {
-        fetch(`http://localhost:3000/recipes${currentRecipe.id}`, {
-            'method': "PATCH",
-            'header': {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            'body': JSON.stringify({
-                'ratings': ++currentRecipe.ratings,
-                'stars': currentRecipe.stars + ratingValue
-            })   
-        }).then(response => response.json())
-        .then(data => {return(data)})
-        .catch(error => alert(error))
-    
-        stars.textContent = `${currentRecipe.stars / currentRecipe.ratings} Stars`
-    }
-    else {
-        console.log("Please enter a number between 0 and 5")
-    }
-})
-
 // Comments form event listener to submit comments, and add them to the database for each recipe
 commentForm.addEventListener('submit', e => {
     e.preventDefault()
-    console.log('clicked')
-    console.log(e.target.value)
-
+    // console.log('clicked')
+    // console.log(e.target.value)
     if (currentRecipe.id !== undefined) {
         fetch(`http://localhost:3000/comments`, {
             'method': "POST",
@@ -205,19 +231,19 @@ commentForm.addEventListener('submit', e => {
                 'refno': currentRecipe.id
             })   
         }).then(response => response.json())
-        .then(commentObj => console.log(commentObj))
+        .then(commentObj => {return(commentObj)})
         .catch(error => alert(error))
     }
     else {
         console.log(`Please select a recipe to comment on it`)
     }
-
 })
 
 fetch(`http://localhost:3000/recipes`)
 .then(response => response.json())
 .then(recipeArray => {
-    recipeArray.forEach(recipeObj => addOneRecipe(recipeObj))
+    recipeArray.forEach(recipeObj => addOneRecipe(recipeObj))  
 }).catch(error => alert(error))
 
 // Display a random recipe upon loading page
+
